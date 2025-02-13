c6089b8f2720943a3318ae333e94b2ade0ef0322
Esther0328 <ycai@zagmail.gonzaga.edu>
2025-02-13 02:32:36 -0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,12 +1,55 @@
 #include <vector>
+#include <iostream>
 
+using std::cout;
+using std::vector;
 
 void part1() {
-    //TODO: Implement part 1
+    //inital a int vector called vec
+    vector<int>vec;
+
+    //add 50000 ints to the vector
+    for(int i=0; i<50000; i++){
+        vec.push_back(i);
+    }
+    
+    //record the size and capacity
+    cout << "The size of the vector is: " << vec.size()
+        <<", the capacity of the vector is: " << vec.capacity()
+        <<".\n";
+
+    //resize to half its size
+    vec.resize(vec.size()/2);
+    cout << "After resizing, " 
+        << "The size of the vector is: " << vec.size()
+        <<", the capacity of the vector is: " << vec.capacity()
+        <<".\n";
+
+    //shrink to fit
+    vec.shrink_to_fit();
+    cout << "After shrinking to fit,"
+        << "The size of the vector is: " << vec.size()
+        <<", the capacity of the vector is: " << vec.capacity()
+        <<".\n";
+    
+
 }
 
 void part2() {
-    //TODO: Implement part 2
+    //create vector called boolVec with 10000 ele valued in true
+    vector<bool>boolVec(10000,true);
+
+    //create vector called charVec with 10000 ele valued in a
+    vector<char>charVec(10000, 'a');
+
+    // Calculate memory usage
+    //this part i try the chatgpt to give me a hint for using the capacity to get the accurate memory usage
+    size_t boolMemUsage = sizeof(boolVec) + (boolVec.capacity() / 8);
+    size_t charMemUsage = sizeof(charVec) + (charVec.capacity() * sizeof(char));
+    
+    //print result
+    cout << "Memory usage of bool vector is" << boolMemUsage << "bytes, \n"
+        << "Memory usage of char vector is" << charMemUsage << "bytes. \n ";
 }
 
 int main() {
